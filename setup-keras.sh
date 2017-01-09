cc1d8794994b37f87c081301c07f5ef1a30f2aec
OpenThings <openthings@163.com>
2017-01-09 12:54:03 +0800

    Update setup-keras.sh

--- setup-keras.sh
+++ setup-keras.sh
@@ -3,15 +3,15 @@
 
 echo
 echo "========================================================================"
-echo "Setup caffe within docker container..."
+echo "Setup keras within docker container..."
 echo "by openthings@163.com,2017-01-08."
 echo "Source from https://github.com/openthings/openml"
 echo "------------------------------------------------------------------------"
-echo "Container name: caffe."
+echo "Container name: keras."
 #echo "Jupyter port: 9888"
 #ehco "Access with http://localhost:9888, please operate as notice."
-echo "More info, please visit https://github.com/openthings/caffe"
+echo "Please visit https://keras.io/,https://github.com/fchollet/keras"
 echo "========================================================================"
 echo
 
-docker run --name caffe -ti caffe:cpu python
+docker run --name caffe -ti -p 9887:8888 openthing
